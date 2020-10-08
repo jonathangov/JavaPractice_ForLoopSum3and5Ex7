955eb0f5f38773ab76fbfcbdd1fc07290c4b00a3
Jonathan Govan <jonathan_govan@homedepot.com>
2020-10-08 08:01:48 -0400

    re-pushing up:

--- src/udemy/practice/Main.java
+++ src/udemy/practice/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         int sum = 0;
         int count = 0;
+
         for(int i=0; count < 6; i++) {
             if((i % 3 == 0) && (i % 5 == 0) && i!=0) {
                 sum += i;
